466c8508a54b2ad3d3840c38c08ada28ca326920
Luis Henrique <luishng@gmail.com>
2025-07-25 11:47:26 -0300

    feat: Add react navigation in project

---
+++ src/routes/index.tsx
@@ -0,0 +1,35 @@
+import { Login } from "@/screens/Login"
+import { createStackNavigator } from "@react-navigation/stack"
+import { NavigationContainer } from "@react-navigation/native"
+import { Register } from "@/screens/Register"
+
+export type PublicStackParamsList = {
+  Login: undefined
+  Register: undefined
+}
+
+const NavigationRoutes = () => {
+  const PublicStack = createStackNavigator<PublicStackParamsList>()
+
+  return (
+    <NavigationContainer>
+      <PublicStack.Navigator
+        screenOptions={{
+          headerShown: false
+        }}
+      >
+        <PublicStack.Screen
+          name="Login"
+          component={Login}
+        />
+
+        <PublicStack.Screen
+          name="Register"
+          component={Register}
+        />
+      </PublicStack.Navigator>
+    </NavigationContainer>
+  )
+}
+
+export default NavigationRoutes
