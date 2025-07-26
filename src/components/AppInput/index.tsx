bd775e06c846881a728a70f86331feda3d3cdfd8
Luis Henrique <luishng@gmail.com>
2025-07-26 11:07:42 -0300

    feat: Ending login screen

--- src/components/AppInput/index.tsx
+++ src/components/AppInput/index.tsx
@@ -1,18 +1,18 @@
-import { View, Text, TextInputProps, TouchableOpacity } from "react-native"
-import { Control, Controller, FieldValues, Path } from "react-hook-form"
-import { TextInput } from "react-native-gesture-handler"
-import { MaterialIcons } from "@expo/vector-icons"
+import { View, Text, TextInputProps, TouchableOpacity } from "react-native";
+import { Control, Controller, FieldValues, Path } from "react-hook-form";
+import { TextInput } from "react-native-gesture-handler";
+import { MaterialIcons } from "@expo/vector-icons";
 
-import { colors } from "@/shared/colors"
-import { useRef, useState } from "react"
+import { colors } from "@/shared/colors";
+import { useRef, useState } from "react";
 
-import clsx from "clsx"
+import clsx from "clsx";
 
 interface AppInputParams<T extends FieldValues> extends TextInputProps {
-  control: Control<T>
-  name: Path<T>
-  leftIconName?: keyof typeof MaterialIcons.glyphMap
-  label?: string
+  control: Control<T>;
+  name: Path<T>;
+  leftIconName?: keyof typeof MaterialIcons.glyphMap;
+  label?: string;
 }
 
 export const AppInput = <T extends FieldValues>({
@@ -23,15 +23,15 @@ export const AppInput = <T extends FieldValues>({
   secureTextEntry,
   ...rest
 }: AppInputParams<T>) => {
-  const [isFocused, setIsFocused] = useState(false)
-  const [showText, setShowText] = useState(secureTextEntry)
-  const inputRef = useRef<TextInput>(null)
+  const [isFocused, setIsFocused] = useState(false);
+  const [showText, setShowText] = useState(secureTextEntry);
+  const inputRef = useRef<TextInput>(null);
 
   const checkFocus = () => {
     if (inputRef.current) {
-      setIsFocused(inputRef.current.isFocused())
+      setIsFocused(inputRef.current.isFocused());
     }
-  }
+  };
 
   return (
     <Controller
@@ -39,25 +39,29 @@ export const AppInput = <T extends FieldValues>({
       name={name}
       render={({ field: { onChange, value } }) => {
         return (
-          <View className="w-full m-4">
+          <View className="w-full mt-4">
             {label && (
               <Text
                 className={clsx(
                   "mb-2 mt-3 text-base",
-                  isFocused ? "text-accent-brand" : 'text-gray-600')}
+                  isFocused ? "text-accent-brand" : "text-gray-600"
+                )}
               >
                 {label}
               </Text>
             )}
-            <TouchableOpacity className="flex-row items-center 
-              justify-between border-b-[1px] border-gray-600 px-3 py-2 h-16">
-              {
-                leftIconName && (
-                  <MaterialIcons
-                    name={leftIconName}
-                    color={isFocused ? colors["accent-brand"] : colors.gray[600]} size={24} className="mr-2" />
-                )
-              }
+            <TouchableOpacity
+              className="flex-row items-center 
+              justify-between border-b-[1px] border-gray-600 px-3 py-2 h-16"
+            >
+              {leftIconName && (
+                <MaterialIcons
+                  name={leftIconName}
+                  color={isFocused ? colors["accent-brand"] : colors.gray[600]}
+                  size={24}
+                  className="mr-2"
+                />
+              )}
               <TextInput
                 ref={inputRef}
                 value={value}
@@ -70,22 +74,19 @@ export const AppInput = <T extends FieldValues>({
                 {...rest}
               />
 
-              {
-                secureTextEntry && (
-                  <TouchableOpacity onPress={() => setShowText((prev) => !prev)}>
-                    <MaterialIcons
-                      name={showText ? "visibility" : "visibility-off"}
-                      color={colors.gray[600]}
-                      size={24}
-                    />
-                  </TouchableOpacity>
-                )
-              }
+              {secureTextEntry && (
+                <TouchableOpacity onPress={() => setShowText((prev) => !prev)}>
+                  <MaterialIcons
+                    name={showText ? "visibility" : "visibility-off"}
+                    color={colors.gray[600]}
+                    size={24}
+                  />
+                </TouchableOpacity>
+              )}
             </TouchableOpacity>
           </View>
-        )
+        );
       }}
     />
-
-  )
-}
+  );
+};
