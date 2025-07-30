dc5c3dd6566d34ed7924a03584ffdf4cc2dcaf4d
Luis Henrique <luishng@gmail.com>
2025-07-30 14:29:02 -0300

    feat: Creating AppError Class and add interceptors in axios

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,9 +1,15 @@
-import { Text, View } from "react-native"
+import { useAuth } from "@/context/auth.context";
+import { Text, View, TouchableOpacity } from "react-native";
 
 export const Home = () => {
+  const { handleLogout } = useAuth();
+
   return (
     <View className="flex-1 items-center justify-center">
       <Text>Home Screen</Text>
+      <TouchableOpacity onPress={handleLogout}>
+        <Text>Sair</Text>
+      </TouchableOpacity>
     </View>
-  )
-}
+  );
+};
