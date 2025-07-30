18cd30b7aebb5fc1d44fe4edc543069304dc790f
Luis Henrique <luishng@gmail.com>
2025-07-30 15:18:40 -0300

    feat: Creating AppHeader

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,15 +1,18 @@
+import { AppHeader } from "@/components/AppHeader";
 import { useAuth } from "@/context/auth.context";
-import { Text, View, TouchableOpacity } from "react-native";
+import { Text, TouchableOpacity } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export const Home = () => {
   const { handleLogout } = useAuth();
 
   return (
-    <View className="flex-1 items-center justify-center">
+    <SafeAreaView className="flex-1 bg-background-primary">
+      <AppHeader />
       <Text>Home Screen</Text>
       <TouchableOpacity onPress={handleLogout}>
         <Text>Sair</Text>
       </TouchableOpacity>
-    </View>
+    </SafeAreaView>
   );
 };
