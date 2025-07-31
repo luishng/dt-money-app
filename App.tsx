7cd1cc6d3346ac171c3cd3d5e21cf0d70425ba19
Luis Henrique <luishng@gmail.com>
2025-07-31 11:44:48 -0300

    feat: Creating context and service of transactions for fetch transactions categories

--- App.tsx
+++ App.tsx
@@ -7,16 +7,19 @@ import { SnackbarContextProvider } from "@/context/snackbar.context";
 import { SnackBar } from "@/components/Snackbar";
 import { BottomSheetProvider } from "@/context/bottomsheet.context";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { TransactionContextProvider } from "@/context/transaction.context";
 
 export default function App() {
   return (
     <GestureHandlerRootView className="flex-1">
       <SnackbarContextProvider>
         <AuthContextProvider>
-          <BottomSheetProvider>
-            <NavigationRoutes />
-            <SnackBar />
-          </BottomSheetProvider>
+          <TransactionContextProvider>
+            <BottomSheetProvider>
+              <NavigationRoutes />
+              <SnackBar />
+            </BottomSheetProvider>
+          </TransactionContextProvider>
         </AuthContextProvider>
       </SnackbarContextProvider>
     </GestureHandlerRootView>
